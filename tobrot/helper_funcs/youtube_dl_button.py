22c6b9af69ea943ba11f3ba03a41500766b96988
tilonuz99 <59096186+tilonuz99@users.noreply.github.com>
2020-04-08 07:26:27 +0500

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -100,7 +100,7 @@ async def youtube_dl_call_back(bot, update):
         return
     #
     await bot.edit_message_text(
-        text="trying to download",
+        text="Yuklanmoqda...",
         chat_id=update.message.chat.id,
         message_id=update.message.message_id
     )
