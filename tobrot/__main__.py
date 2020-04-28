ba488f22dc0c6a5a154679af9321b166dbd964e5
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-28 10:47:26 +0500

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -18,8 +18,7 @@ from tobrot import (
     DOWNLOAD_LOCATION,
     TG_BOT_TOKEN,
     APP_ID,
-    API_HASH,
-    AUTH_CHANNEL
+    API_HASH
 )
 
 from pyrogram import Client, Filters, MessageHandler, CallbackQueryHandler
@@ -54,13 +53,13 @@ if __name__ == "__main__" :
     #
     incoming_message_handler = MessageHandler(
         incoming_message_f,
-        filters=Filters.command(["leech"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["leech"])
     )
     app.add_handler(incoming_message_handler)
     #
     incoming_youtube_dl_handler = MessageHandler(
         incoming_youtube_dl_f,
-        filters=Filters.command(["ytdl"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["ytdl"])
     )
     app.add_handler(incoming_youtube_dl_handler)
     #
@@ -72,7 +71,7 @@ if __name__ == "__main__" :
     #
     cancel_message_handler = MessageHandler(
         cancel_message_f,
-        filters=Filters.command(["cancel"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["cancel"])
     )
     app.add_handler(cancel_message_handler)
     #
@@ -90,25 +89,24 @@ if __name__ == "__main__" :
     #
     upload_document_handler = MessageHandler(
         upload_document_f,
-        filters=Filters.command(["upload"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["upload"])
     )
     app.add_handler(upload_document_handler)
 
     help_text_handler = MessageHandler(
         help_message_f,
-        filters=Filters.command(["help"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["help"])
     )
     app.add_handler(help_text_handler)
     #
     new_join_handler = MessageHandler(
-        new_join_f,
-        filters=~Filters.chat(chats=AUTH_CHANNEL)
+        new_join_f
     )
     app.add_handler(new_join_handler)
     #
     group_new_join_handler = MessageHandler(
         help_message_f,
-        filters=Filters.chat(chats=AUTH_CHANNEL) & Filters.new_chat_members
+        filters=Filters.new_chat_members
     )
     app.add_handler(group_new_join_handler)
     #
@@ -119,13 +117,13 @@ if __name__ == "__main__" :
     #
     save_thumb_nail_handler = MessageHandler(
         save_thumb_nail,
-        filters=Filters.command(["savethumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["savethumbnail"])
     )
     app.add_handler(save_thumb_nail_handler)
     #
     clear_thumb_nail_handler = MessageHandler(
         clear_thumb_nail,
-        filters=Filters.command(["clearthumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["clearthumbnail"])
     )
     app.add_handler(clear_thumb_nail_handler)
     #
